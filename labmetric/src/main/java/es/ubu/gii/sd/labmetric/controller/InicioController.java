34b4ca24d972be9b61446e1255e14f66df5c4f8d
MJGomezLopez <128587635+MJGomezLopez@users.noreply.github.com>
2025-06-03 00:23:32 +0200

    Creada la pagina de inicio

--- labmetric/src/main/java/es/ubu/gii/sd/labmetric/controller/InicioController.java
+++ labmetric/src/main/java/es/ubu/gii/sd/labmetric/controller/InicioController.java
@@ -1,17 +1,18 @@
 package es.ubu.gii.sd.labmetric.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import es.ubu.gii.sd.labmetric.repository.UsuarioRepository;
+
 
 /**
  * Clase controlador para el modelo de usuarios
  */
 @Controller
-public class UsuarioController {
+public class InicioController {
 
-	@Autowired
-	private UsuarioRepository usuRepo;
+	@GetMapping({"/", "/inicio"})
+	public String inicio() {
+		return "inicio";
+	}
 	
 }
