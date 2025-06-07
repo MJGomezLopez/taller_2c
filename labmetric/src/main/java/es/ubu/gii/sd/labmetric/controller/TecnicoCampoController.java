2fe571aa2b5e21ae4fe31407e5562039c8775940
MJGomezLopez <128587635+MJGomezLopez@users.noreply.github.com>
2025-06-08 01:38:58 +0200

    creada la pagina de tecnicos de campo

--- labmetric/src/main/java/es/ubu/gii/sd/labmetric/controller/TecnicoCampoController.java
+++ labmetric/src/main/java/es/ubu/gii/sd/labmetric/controller/TecnicoCampoController.java
@@ -1,7 +1,12 @@
 package es.ubu.gii.sd.labmetric.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import es.ubu.gii.sd.labmetric.model.TecnicoCampo;
 import es.ubu.gii.sd.labmetric.repository.TecnicoCampoRepository;
 
 /**
@@ -12,5 +17,12 @@ public class TecnicoCampoController {
 
 	@Autowired
 	private TecnicoCampoRepository rutaRepo;
+	
+	@GetMapping("/tecnicos")
+	public String listarTecnicos(Model model) {
+	    List<TecnicoCampo> tecnicos = rutaRepo.findAll();
+	    model.addAttribute("tecnicos", tecnicos);
+	    return "tecnicos";
+	}
 
 }
