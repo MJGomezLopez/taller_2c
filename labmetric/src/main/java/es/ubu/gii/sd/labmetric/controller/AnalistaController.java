10be11afa1ee1d76acdbd71e513ec232bf8eae7c
MJGomezLopez <128587635+MJGomezLopez@users.noreply.github.com>
2025-06-08 01:50:09 +0200

    Creada la pagina de analistas

--- labmetric/src/main/java/es/ubu/gii/sd/labmetric/controller/AnalistaController.java
+++ labmetric/src/main/java/es/ubu/gii/sd/labmetric/controller/AnalistaController.java
@@ -1,7 +1,13 @@
 package es.ubu.gii.sd.labmetric.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import es.ubu.gii.sd.labmetric.model.Analista;
 import es.ubu.gii.sd.labmetric.repository.AnalistaRepository;
 
 /**
@@ -12,5 +18,12 @@ public class AnalistaController {
 
 	@Autowired
 	private AnalistaRepository analistaRepo;
+	
+	@GetMapping("/analistas")
+	public String listarAnalistas(Model model) {
+	    List<Analista> analistas = analistaRepo.findAll();
+	    model.addAttribute("analistas", analistas);
+	    return "analistas";
+	}
 
 }
