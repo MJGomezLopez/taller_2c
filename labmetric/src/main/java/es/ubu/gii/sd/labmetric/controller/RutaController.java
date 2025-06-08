96101cfbeec2d953c3e593592f119a5437f221f1
MJGomezLopez <128587635+MJGomezLopez@users.noreply.github.com>
2025-06-08 02:19:55 +0200

    Creada la pagina de rutas

--- labmetric/src/main/java/es/ubu/gii/sd/labmetric/controller/RutaController.java
+++ labmetric/src/main/java/es/ubu/gii/sd/labmetric/controller/RutaController.java
@@ -1,7 +1,13 @@
 package es.ubu.gii.sd.labmetric.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import es.ubu.gii.sd.labmetric.model.Ruta;
 import es.ubu.gii.sd.labmetric.repository.RutaRepository;
 
 /**
@@ -12,5 +18,12 @@ public class RutaController {
 
 	@Autowired
 	private RutaRepository rutaRepo;
+	
+    @GetMapping("/rutas")
+    public String listarRutas(Model model) {
+        List<Ruta> rutas = rutaRepo.findAll();
+        model.addAttribute("rutas", rutas);
+        return "rutas"; // corresponde a rutas.html
+    }
 
 }
