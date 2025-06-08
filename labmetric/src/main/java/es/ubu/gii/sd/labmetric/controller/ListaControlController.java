c75ec893de56f0e442c0bd27ce63846b30f1fcff
MJGomezLopez <128587635+MJGomezLopez@users.noreply.github.com>
2025-06-08 09:58:13 +0200

    Creada la lista de control

--- labmetric/src/main/java/es/ubu/gii/sd/labmetric/controller/ListaControlController.java
+++ labmetric/src/main/java/es/ubu/gii/sd/labmetric/controller/ListaControlController.java
@@ -2,6 +2,8 @@ package es.ubu.gii.sd.labmetric.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import es.ubu.gii.sd.labmetric.repository.ListaControlRepository;
 
 /**
@@ -12,5 +14,11 @@ public class ListaControlController {
 
 	@Autowired
 	private ListaControlRepository listaRepo;
+	
+    @GetMapping("/listaControl")
+    public String verListaControl(Model model) {
+        model.addAttribute("registros", listaRepo.findAll());
+        return "listaControl";
+    }
 
 }
