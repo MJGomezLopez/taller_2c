695c50e1868792da09ae52b1b7e41eb6411e43ba
MJGomezLopez <mgl1025@alu.ubu.es>
2025-06-09 00:12:09 +0200

    creada pagina para crear puntos de muestreo

--- labmetric/src/main/java/es/ubu/gii/sd/labmetric/controller/PuntoMuestreoController.java
+++ labmetric/src/main/java/es/ubu/gii/sd/labmetric/controller/PuntoMuestreoController.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import es.ubu.gii.sd.labmetric.model.PuntoMuestreo;
 import es.ubu.gii.sd.labmetric.repository.PuntoMuestreoRepository;
+import es.ubu.gii.sd.labmetric.repository.RutaRepository;
 
 /**
  * Clase controlador para el modelo de punto de muestreos
@@ -16,11 +18,26 @@ public class PuntoMuestreoController {
 	@Autowired
 	private PuntoMuestreoRepository pmRepo;
 	
+	@Autowired
+	private RutaRepository rutaRepo;
+	
 	@GetMapping("/puntosDeMuestreo")
 	public String listarPuntosDeMuestreo(Model model) {
 	    model.addAttribute("puntos", pmRepo.findAll());
 	    return "puntos_muestreo";
 	}
-
 	
+	@GetMapping("/puntosDeMuestreo/nuevo")
+	public String mostrarFormularioNuevoPunto(Model model) {
+	    model.addAttribute("puntoMuestreo", new PuntoMuestreo());
+	    model.addAttribute("rutas", rutaRepo.findAll());
+	    return "puntos_muestreo_nuevo";
+	}
+
+	@PostMapping("/puntosDeMuestreo/nuevo")
+	public String guardarPuntoMuestreo(@ModelAttribute PuntoMuestreo puntoMuestreo) {
+	    pmRepo.save(puntoMuestreo);
+	    return "redirect:/puntosDeMuestreo";
+	}
+
 }
