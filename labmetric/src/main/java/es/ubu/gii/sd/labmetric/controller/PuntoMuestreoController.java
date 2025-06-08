d00da3623a31c26945e50f79b1b0ca01770c75b7
MJGomezLopez <128587635+MJGomezLopez@users.noreply.github.com>
2025-06-08 09:43:54 +0200

    Creada la pagina de puntos de muestreo

--- labmetric/src/main/java/es/ubu/gii/sd/labmetric/controller/PuntoMuestreoController.java
+++ labmetric/src/main/java/es/ubu/gii/sd/labmetric/controller/PuntoMuestreoController.java
@@ -2,6 +2,9 @@ package es.ubu.gii.sd.labmetric.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
 import es.ubu.gii.sd.labmetric.repository.PuntoMuestreoRepository;
 
 /**
@@ -12,5 +15,12 @@ public class PuntoMuestreoController {
 	
 	@Autowired
 	private PuntoMuestreoRepository pmRepo;
+	
+	@GetMapping("/puntosDeMuestreo")
+	public String listarPuntosDeMuestreo(Model model) {
+	    model.addAttribute("puntos", pmRepo.findAll());
+	    return "puntos_muestreo";
+	}
+
 	
 }
